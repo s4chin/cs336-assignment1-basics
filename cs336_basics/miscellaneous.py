dd60a27e21dbd10b6eb38b3b61e79f12f111153f
Sachin Kelkar <kelkar@adobe.com>
2025-06-15 15:10:58 -0500

    grad clipping fix

--- cs336_basics/miscellaneous.py
+++ cs336_basics/miscellaneous.py
@@ -3,13 +3,19 @@ from collections.abc import Iterable
 
 
 def gradient_clipping(parameters: Iterable[torch.nn.Parameter], max_l2_norm: float, eps: float = 1e-6):
+    all_norms = []
     for p in parameters:
         if p.grad is None:
             continue
         in_dtype = p.grad.data.dtype
         norm = torch.norm(p.grad.data, 2)
-        if norm < max_l2_norm:
-            continue
-
-        p.grad.data = p.grad.data * (max_l2_norm / (norm + eps))
-        p.grad.data = p.grad.data.to(in_dtype)
+        all_norms.append(norm ** 2)
+    
+    total_norm = torch.sqrt(sum(all_norms))
+    if total_norm > max_l2_norm:
+        scale = max_l2_norm / (total_norm + eps)
+        for p in parameters:
+            if p.grad is None:
+                continue
+            p.grad.data = p.grad.data * scale
+            p.grad.data = p.grad.data.to(in_dtype)
